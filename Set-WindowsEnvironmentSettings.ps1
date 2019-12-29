e8ab7bcf618bac9e094b4d08dbe49acbd627ef5c
Rafael Julio <hello.rjulio@gmail.com>
2019-12-29 14:04:48 -0300

    refactor(windows-environment): use #Requires statements to check for elevated permissions

--- Set-WindowsEnvironmentSettings.ps1
+++ Set-WindowsEnvironmentSettings.ps1
@@ -1,7 +1,5 @@
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`[Security.Principal.WindowsBuiltInRole] “Administrator”)) {
-  Write-Warning “You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!”
-  Break
-}
+#Requires -RunAsAdministrator
+#Requires -Version 4.0
 
 # Uninstall OneDrive
 $OneDriveProcess = Get-Process OneDrive -ErrorAction SilentlyContinue
