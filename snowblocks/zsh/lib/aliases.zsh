8a6ddf5ef5c305da0ecdb439a8c125c66adc26e3
Rafael Julio <hello.rjulio@gmail.com>
2021-02-07 16:09:02 -0300

    refactor(zsh): aliases: replace axel with httpd

--- snowblocks/zsh/lib/aliases.zsh
+++ snowblocks/zsh/lib/aliases.zsh
@@ -5,14 +5,14 @@
 # License:    MIT License
 
 # Downloads a remote file using aria2c with opinionated fallbacks.
-# The order of preference is: aria2c, axel, wget, curl.
+# The order of preference is: aria2c, http, wget, curl.
 # This order is derrived from speed based on personal tests.
 # See:
 #   1. https://github.com/zimfw/utility/blob/5fc2348ff5688972cdc87a2010796525e9656966/init.zsh#L32
 if (( ${+commands[aria2c]} )); then
-  alias get='aria2c --max-connection-per-server=5 --continue'
-elif (( ${+commands[axel]} )); then
-  alias get='axel --num-connections=5 --alternate'
+  alias get='aria2c --max-connection-per-server=8 --continue'
+elif (( ${+commands[http]} )); then
+  alias get='http --check-status --follow --download --continue'
 elif (( ${+commands[wget]} )); then
   alias get='wget --continue --progress=bar --timestamping'
 elif (( ${+commands[curl]} )); then
