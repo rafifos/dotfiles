fca9324202507298cb890d0c4f41c7dbf96c5c76
Rafael Julio <hello.rjulio@gmail.com>
2021-02-09 01:27:50 -0300

    feature: bundle exa

---
+++ snowblocks/zsh/pkgs/exa/aliases.zsh
@@ -0,0 +1,8 @@
+# Copyright (c) 2021 Rafael Julio <development@rafifos.dev>
+
+# Project:    dotfiles
+# Repository: https://github.com/rafifos/dotfiles
+# License:    MIT License
+
+# Adds extra verbosity to exa.
+alias exa='exa --long --header --all --extended --git --group-directories-first --time-style=long-iso'
