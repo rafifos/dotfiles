59fe07e4a0a044e1bfe96b90c0a47171f6962fe1
Rafael Julio <rafael.julio@mesalva.com>
2021-03-01 16:03:14 -0300

    feat(fish): implement resetenv function

---
+++ linux/fish/functions/resetenv.fish
@@ -0,0 +1,3 @@
+function resetenv --description "Resets the environment (by deleting $__fish_user_config_dir/fish_variables)."
+    test -f $__fish_user_config_dir/fish_variables; and rm $__fish_user_config_dir/fish_variables; and echo 'Done.'
+end
