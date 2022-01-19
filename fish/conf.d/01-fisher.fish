68294940b8c504a0e7126cb63ce77e043b369e20
Rafael Julio <rafael.julio@mesalva.com>
2022-01-19 11:57:54 -0300

    fix(fish): fix fisher init
    Signed-off-by: Rafael Julio <rafael.julio@mesalva.com>

--- fish/conf.d/01-fisher.fish
+++ fish/conf.d/01-fisher.fish
@@ -1,11 +1,4 @@
-# First boot, install and setup plugins.
-if status is-interactive; and not type -q fisher
-    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-
-    fisher install b4b4r07/enhancd
-    fisher install gazorby/fish-abbreviation-tips
-    fisher install jorgebucaran/autopair.fish
-    fisher install jorgebucaran/replay.fish
-    fisher install oh-my-fish/plugin-grc
-    fisher install wfxr/forgit
+# Installs fisher if it isn't installed.
+if status is-interactive && ! functions -q fisher
+    curl -sL https://git.io/fisher | source && fisher update
 end
