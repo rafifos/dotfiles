3652cd4f071b579a8a4d32b1659babd4c6b980ff
Rafael Julio <rafael.julio@mesalva.com>
2022-01-19 12:02:02 -0300

    chore(asdf): update install steps
    Signed-off-by: Rafael Julio <rafael.julio@mesalva.com>

--- fish/conf.d/02-asdf.fish
+++ fish/conf.d/02-asdf.fish
@@ -1,7 +1,7 @@
 # asdf-vm setup.
 # See: https://asdf-vm.com/#/core-manage-asdf?id=install
 if not test -d ~/.asdf
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
 
     not test -d ~/.config/fish/completions; and mkdir -p ~/.config/fish/completions
     ln -sf ~/.asdf/completions/asdf.fish ~/.config/fish/completions
@@ -14,9 +14,12 @@ if not test -d ~/.asdf
     # Post-install tasks.
     replay 'bash -c \'${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring\''
 
+    asdf install nodejs lts
     asdf install nodejs lts-fermium
-    asdf global nodejs lts-fermium
+    asdf install nodejs lts-erbium
     asdf install yarn latest
+
+    asdf global nodejs lts
     asdf global yarn latest
 else
     source ~/.asdf/asdf.fish
