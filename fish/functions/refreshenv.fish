8d8b538012739c21710a98adc08624717eece2bb
Rafael Julio <rafael.julio@mesalva.com>
2022-02-17 17:02:30 -0300

    fix(fish): use `command` for rm
    Signed-off-by: Rafael Julio <rafael.julio@mesalva.com>

--- fish/functions/refreshenv.fish
+++ fish/functions/refreshenv.fish
@@ -1,3 +1,3 @@
 function refreshenv --description "Resets the environment (by deleting $__fish_user_config_dir/fish_variables)."
-    test -f $__fish_user_config_dir/fish_variables; and rm $__fish_user_config_dir/fish_variables; and echo 'Done.'
+    test -f $__fish_user_config_dir/fish_variables; and command rm $__fish_user_config_dir/fish_variables; and echo 'Done.'
 end
