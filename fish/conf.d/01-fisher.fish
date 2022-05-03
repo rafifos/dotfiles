77e35e4febb1fd49bbaf6b41ee13c46363fe913a
Rafael Julio <rafael.julio@mesalva.com>
2022-05-03 17:56:52 -0300

    refactor(fish): update install url

--- fish/conf.d/01-fisher.fish
+++ fish/conf.d/01-fisher.fish
@@ -1,4 +1,4 @@
 # Installs fisher if it isn't installed.
 if status is-interactive && ! functions -q fisher
-    curl -sL https://git.io/fisher | source && fisher update
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
 end
