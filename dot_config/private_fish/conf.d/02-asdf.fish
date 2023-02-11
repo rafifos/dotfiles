a265a3613fd8f87c55a1cdd12f7fb7b978cee68e
Rafael Julio <development@rafifos.dev>
2023-02-11 01:48:56 +0000

    refactor: reorganize asdf install
    Signed-off-by: Rafael Julio <development@rafifos.dev>

--- dot_config/private_fish/conf.d/02-asdf.fish
+++ dot_config/private_fish/conf.d/02-asdf.fish
@@ -1,7 +1,7 @@
 # asdf-vm setup.
 # See: https://asdf-vm.com/#/core-manage-asdf?id=install
 if not test -d ~/.asdf
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.1
 
     not test -d ~/.config/fish/completions; and mkdir -p ~/.config/fish/completions
     ln -sf ~/.asdf/completions/asdf.fish ~/.config/fish/completions
@@ -9,19 +9,11 @@ if not test -d ~/.asdf
     source ~/.asdf/asdf.fish
 
     asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-    asdf plugin-add yarn https://github.com/twuni/asdf-yarn.git
-
-    # Post-install tasks.
-    replay 'bash -c \'${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring\''
 
     asdf install nodejs lts
-    asdf install nodejs lts-erbium
-    asdf install nodejs lts-fermium
-    asdf install nodejs lts-gallium
-    asdf install yarn latest
+    asdf install nodejs lts-hydrogen
 
     asdf global nodejs lts
-    asdf global yarn latest
 else
     source ~/.asdf/asdf.fish
 end
