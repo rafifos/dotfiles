ef5fefb2830cdc4b252058e31dfba2d24410459b
Rafael Julio Lemos Silva <rafael.lemos@aec.com.br>
2023-07-13 17:58:20 -0300

    fix(fish): fix asdf node.js setup

--- dot_config/private_fish/conf.d/02-asdf.fish
+++ dot_config/private_fish/conf.d/02-asdf.fish
@@ -1,7 +1,7 @@
 # asdf-vm setup.
 # See: https://asdf-vm.com/#/core-manage-asdf?id=install
 if not test -d ~/.asdf
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.1
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
 
     not test -d ~/.config/fish/completions; and mkdir -p ~/.config/fish/completions
     ln -sf ~/.asdf/completions/asdf.fish ~/.config/fish/completions
@@ -10,10 +10,9 @@ if not test -d ~/.asdf
 
     asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
-    asdf install nodejs lts
-    asdf install nodejs lts-hydrogen
-
-    asdf global nodejs lts
+    # https://github.com/asdf-vm/asdf-nodejs/issues/352#issuecomment-1569477783
+    asdf install nodejs (asdf nodejs resolve lts --latest-available)
+    asdf global nodejs (asdf nodejs resolve lts --latest-available)
 else
     source ~/.asdf/asdf.fish
 end
