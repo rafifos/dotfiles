f5f46cf05e3c41b5f4da99887f0f9c73b9ab8e6e
Rafael Julio <development@rafifos.dev>
2025-02-24 14:14:25 -0300

    fix(fish): enhance deprecation warning formatting for better visibility
    Signed-off-by: Rafael Julio <development@rafifos.dev>

--- dot_config/private_fish/functions/_deprecate_command.fish
+++ dot_config/private_fish/functions/_deprecate_command.fish
@@ -1,8 +1,16 @@
 function _deprecate_command --description 'Deprecates a command by printing a message after running it.'
-  if status --is-interactive
-    set -l command $argv[1]
-    set -l alternative $argv[2]
+    if status --is-interactive
+        set -l command $argv[1]
+        set -l alternative $argv[2]
 
-    echo "Deprecation warning: $command is deprecated. Use $alternative instead."
-  end
+        echo -n "Deprecation warning: "
+        set_color $fish_color_error
+        echo -n "$command "
+        set_color $fish_color_normal
+        echo -n "is deprecated. Use "
+        set_color $fish_color_command
+        echo -n "$alternative "
+        set_color $fish_color_normal
+        echo "instead."
+    end
 end
