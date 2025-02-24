699d7686871cdb6f139d54cd188037c03d26bed0
Rafael Julio <development@rafifos.dev>
2025-02-24 14:00:08 -0300

    fix(fish): simplify fundle installation check
    Signed-off-by: Rafael Julio <development@rafifos.dev>

--- dot_config/private_fish/conf.d/01-fundle.fish
+++ dot_config/private_fish/conf.d/01-fundle.fish
@@ -1,5 +1,5 @@
 # Installs fundle if it isn't installed.
-if status is-interactive; and not functions -q fundle
+if not functions -q fundle
     eval (curl -sfL https://git.io/fundle-install)
 end
 
